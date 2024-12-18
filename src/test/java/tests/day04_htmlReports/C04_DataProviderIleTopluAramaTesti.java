c21eede208143fffdda4b5c164b79f2c0502f856
HamzaKAVAS <hmzkavas@gmail.com>
2024-12-19 00:02:06 +0300

    dataProvider

--- src/test/java/tests/day04_htmlReports/C04_DataProviderIleTopluAramaTesti.java
+++ src/test/java/tests/day04_htmlReports/C04_DataProviderIleTopluAramaTesti.java
@@ -1,6 +1,34 @@
 package tests.day04_htmlReports;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class C04_DataProviderIleTopluAramaTesti {
 
+    @DataProvider
+    public static Object[][] UrunProvideri() {
+        String[][] urunlerArrayi = { {"phone"}, {"java"}, {"dress"}, {"baby"}, {"nutella"}, {"samsung"}, {"iphone"}, {"shirt"} };
+        return urunlerArrayi;
+    }
 
+    @Test(dataProvider = "UrunProvideri")
+    public void aramaTesti(String aranacakUrun) {
+        // testotomasyonu sayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+
+        // arama kutusuna aranacakUrun'u yazdirip enter'a basin
+        testotomasyonuPage.aramaKutusu.sendKeys(aranacakUrun + Keys.ENTER);
+
+        // arama sonucunda urun bulunabildigini test edin
+        String uxExpectedAramaSonucu = ConfigReader.getProperty("toUnexpectedSonuc");
+        String actualAramaSonucuYazisi = testotomasyonuPage.aramaSonucElementi.getText();
+        Assert.assertNotEquals(actualAramaSonucuYazisi, uxExpectedAramaSonucu, aranacakUrun + " bulunamadı.");
+        Driver.quitDriver();
+    }
 }
