8a9bbf356474effd817b0129f0004a1b143014a8
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-11 23:25:45 +0300

    JScriptAlert

---
+++ src/test/java/pages/TOJScriptPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class TOJScriptPage {
+
+    public TOJScriptPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//*[@*='j-button'])[1]")
+    public WebElement jsAlertButonu;
+
+    @FindBy(xpath = "(//*[@*='j-button'])[2]")
+    public WebElement jsConfirmButonu;
+
+    @FindBy(xpath = "(//*[@*='j-button'])[3]")
+    public WebElement jsPromptButonu;
+
+
+}
---
+++ src/test/java/tests/practices/P17_JScriptAlert.java
@@ -0,0 +1,49 @@
+package tests.practices;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TOJScriptPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class P17_JScriptAlert {
+
+    // 1. Test
+    //   - https://testotomasyonu.com/javascriptAlert adresine gidin
+    //   - 1.alert'e tiklayin
+    //   - Alert'deki yazinin "I am a JS Alert" oldugunu test edin
+    //   - OK tusuna basip alert'i kapatin
+    // 2.Test
+    //   - https://testotomasyonu.com/javascriptAlert adresine gidin
+    //   - 2.alert'e tiklayalim
+    //   - Cancel'a basip, cikan sonuc yazisinin "You clicked: Cancel" oldugunu test edin
+    // 3.Test
+    //   - https://testotomasyonu.com/javascriptAlert adresine gidin
+    //   - 3.alert'e tiklayalim
+    //   - Cikan prompt ekranina "Abdullah" yazdiralim
+    //   - OK tusuna basarak alert'i kapatalim
+    //   - Cikan sonuc yazisinin Abdullah icerdigini test edelim
+
+    @Test
+    public void test01() {
+        // 1. Test
+        //   - https://testotomasyonu.com/javascriptAlert adresine gidin
+        Driver.getDriver().get(ConfigReader.getProperty("toJScript"));
+        TOJScriptPage tojScriptPage = new TOJScriptPage();
+
+        //   - 1.alert'e tiklayin
+        tojScriptPage.jsAlertButonu.click();
+
+        //   - Alert'deki yazinin "I am a JS Alert" oldugunu test edin
+        String expectedAlertYazi = "I am a JS Alert";
+        String actualAlertYazi = Driver.getDriver().switchTo().alert().getText();
+        Assert.assertEquals(actualAlertYazi, expectedAlertYazi);
+
+        //   - OK tusuna basip alert'i kapatin
+        Driver.getDriver().switchTo().alert().accept();
+        Driver.quitDriver();
+    }
+
+
+}
