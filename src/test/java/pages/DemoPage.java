92d5901b07686af74ab808980336d559e0807150
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-13 23:39:27 +0300

    Iframe

--- src/test/java/pages/DemoPage.java
+++ src/test/java/pages/DemoPage.java
@@ -1,13 +1,30 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class DemoPage {
 
-    public DemoPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public DemoPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(xpath = "//iframe")
+    public List<WebElement> iframes;
 
+    @FindBy(xpath = "(//iframe)[1]")
+    public WebElement youtubeIframe;
+
+    @FindBy(id = "movie_player")
+    public WebElement youtubePlayElement;
+
+    @FindBy(xpath = "//*[@*='http://www.guru99.com/live-selenium-project.html']")
+    public WebElement jMeterLinki;
+
+    @FindBy(xpath = "(//iframe)[2]")
+    public WebElement jMeterIframe;
 }
--- src/test/java/tests/practices/P19_Iframe.java
+++ src/test/java/tests/practices/P19_Iframe.java
@@ -1,8 +1,13 @@
 package tests.practices;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import pages.DemoPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class P19_Iframe {
 
@@ -13,14 +18,29 @@ public class P19_Iframe {
     // 5) ikinci iframe'deki (Jmeter Made Easy) linke (https://www.guru99.com/live-selenium-project.html) tıklayınız
 
     @Test
-    public void demoIframeTest(){
+    public void demoIframeTest() {
         // 1) http://demo.guru99.com/test/guru99home/ sitesine gidiniz
         Driver.getDriver().get(ConfigReader.getProperty("demoIframeUrl"));
+        DemoPage demoPage = new DemoPage();
 
         // 2) sayfadaki iframe sayısını bulunuz.
+        System.out.println("Sayfadaki Iframe miktarı: " + demoPage.iframes.size());
+
         // 3) ilk iframe'deki (Youtube) play butonuna tıklayınız.
+        Driver.getDriver().switchTo().frame(demoPage.youtubeIframe);
+        demoPage.youtubePlayElement.click();
+
         // 4) ilk iframe'den çıkıp ana sayfaya dönünüz
+        Driver.getDriver().switchTo().defaultContent();
+
         // 5) ikinci iframe'deki (Jmeter Made Easy) linke (https://www.guru99.com/live-selenium-project.html) tıklayınız
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Driver.getDriver().switchTo().frame(demoPage.jMeterIframe);
+        demoPage.jMeterLinki.click();
         Driver.quitDriver();
     }
 }
