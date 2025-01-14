d3443525d01b1c14006a7d48074b73a4ed30dd78
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-14 22:23:15 +0300

    WindowHandle

--- src/test/java/tests/practices/P20_WindowHandles.java
+++ src/test/java/tests/practices/P20_WindowHandles.java
@@ -1,9 +1,13 @@
 package tests.practices;
 
 import org.openqa.selenium.WindowType;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.util.Set;
 
 public class P20_WindowHandles {
 
@@ -11,31 +15,47 @@ public class P20_WindowHandles {
     // 2- Sayfa’nin window handle degerini String bir degiskene atayin
     // 3- Sayfa title’nin “Otomasyon” icerdigini test edin
     // 4- Yeni bir tab olusturup, acilan tab’da wisequarter.com adresine gidin
-    // 5- Sayfa title’nin “wisequarter” icerdigini test edin
-    // 6- Yeni bir window olusturup, acilan sayfada walmart.com adresine gidin
-    // 7- Sayfa title’nin “Walmart” icerdigini test edin
-    // 8- Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+    // 5- Sayfa title’nin “Wise Quarter” icerdigini test edin
+    // 6- Yeni bir window olusturup, acilan sayfada youtube.com adresine gidin
+    // 7- Sayfa title’nin “YouTube” icerdigini test edin
+    // 8- Ilk acilan sayfaya donun ve testotomasyonu sayfasina dondugunuzu test edin
 
     @Test
-    public void windowHandleTest(){
+    public void windowHandleTest() {
         // 1- Testotomasyonu anasayfa adresine gidin.
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
 
         // 2- Sayfa’nin window handle degerini String bir degiskene atayin
-        String toWindowHandle = Driver.getDriver().getWindowHandle();
+        String ilkSayfaHandleDegeri = Driver.getDriver().getWindowHandle();
+        System.out.println("Testotomasyonu window handle değeri: " + ilkSayfaHandleDegeri);
 
         // 3- Sayfa title’nin “Otomasyon” icerdigini test edin
         String expectedTitleIcerik = "Otomasyon";
         String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitleIcerik));
 
         // 4- Yeni bir tab olusturup, acilan tab’da wisequarter.com adresine gidin
         Driver.getDriver().switchTo().newWindow(WindowType.TAB).get(ConfigReader.getProperty("wiseUrl"));
 
-        // 5- Sayfa title’nin “wisequarter” icerdigini test edin
-        // 6- Yeni bir window olusturup, acilan sayfada walmart.com adresine gidin
-        // 7- Sayfa title’nin “Walmart” icerdigini test edin
-        // 8- Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        // 5- Sayfa title’nin “Wise Quarter” icerdigini test edin
+        expectedTitleIcerik = "Wise Quarter";
+        actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitleIcerik));
 
+        // 6- Yeni bir window olusturup, acilan sayfada youtube.com adresine gidin
+        Driver.getDriver().switchTo().newWindow(WindowType.WINDOW).get(ConfigReader.getProperty("youtubeUrl"));
+
+        // 7- Sayfa title’nin “YouTube” icerdigini test edin
+        expectedTitleIcerik = "YouTube";
+        actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitleIcerik));
+
+        // 8- Ilk acilan sayfaya donun ve testotomasyonu sayfasina dondugunuzu test edin
+        ReusableMethods.bekle(1);
+        Driver.getDriver().switchTo().window(ilkSayfaHandleDegeri);
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://www.testotomasyonu.com/";
+        Assert.assertEquals(actualUrl, expectedUrl);
         Driver.quitDriver();
     }
 }
