af5ac6be9fe86892f89a45336ed425777cf2348c
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-20 22:06:21 +0300

    actions

--- src/test/java/tests/practices/P24_ActionClass.java
+++ src/test/java/tests/practices/P24_ActionClass.java
@@ -1,8 +1,11 @@
 package tests.practices;
 
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import pages.HerOkuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class P24_ActionClass {
 
@@ -17,8 +20,13 @@ public class P24_ActionClass {
     public void actionClassTest(){
         // 1- https://the-internet.herokuapp.com/context_menu sitesine gidin
         Driver.getDriver().get(ConfigReader.getProperty("herOkuActionsUrl"));
+        HerOkuPage herOkuPage = new HerOkuPage();
 
         // 2- Cizili alan uzerinde sag click yapin
+        Actions actions = new Actions(Driver.getDriver());
+        actions.contextClick(herOkuPage.ciziliAlanElementi).perform();
+        ReusableMethods.bekle(2);
+
         // 3- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
         // 4- Tamam diyerek alert’i kapatalim
         // 5- Elemental Selenium linkine tiklayalim
