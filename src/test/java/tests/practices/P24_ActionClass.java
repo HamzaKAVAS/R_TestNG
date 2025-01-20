cd10954e53b5013367d8704b6a42871af0754f34
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-20 22:10:34 +0300

    actions

--- src/test/java/tests/practices/P24_ActionClass.java
+++ src/test/java/tests/practices/P24_ActionClass.java
@@ -1,6 +1,7 @@
 package tests.practices;
 
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HerOkuPage;
 import utilities.ConfigReader;
@@ -25,10 +26,16 @@ public class P24_ActionClass {
         // 2- Cizili alan uzerinde sag click yapin
         Actions actions = new Actions(Driver.getDriver());
         actions.contextClick(herOkuPage.ciziliAlanElementi).perform();
-        ReusableMethods.bekle(2);
 
         // 3- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
+        String expectedAlertText = "You selected a context menu";
+        String actualAlertText = Driver.getDriver().switchTo().alert().getText();
+        Assert.assertEquals(actualAlertText,expectedAlertText);
+
         // 4- Tamam diyerek alert’i kapatalim
+        Driver.getDriver().switchTo().alert().accept();
+        ReusableMethods.bekle(2);
+
         // 5- Elemental Selenium linkine tiklayalim
         // 6- Acilan sayfada h1 taginda “Make sure your code lands” yazdigini test edelim
         Driver.quitDriver();
