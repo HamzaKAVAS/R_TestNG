ac9d4b477b4149e12127ab3501c7af9dca0e4b6d
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-25 15:59:51 +0300

    fileUpload

---
+++ src/test/java/tests/practices/P25_FileUpload.java
@@ -0,0 +1,21 @@
+package tests.practices;
+
+import org.testng.annotations.Test;
+
+public class P25_FileUpload {
+
+    // 1. https://the-internet.herokuapp.com/upload adresine gidelim
+    // 2. chooseFile butonuna basalim
+    // 3. Yuklemek istediginiz dosyayi secelim.
+    // 4. Upload butonuna basalim.
+    // 5. “File Uploaded!” textinin goruntulendigini test edelim.
+
+    @Test
+    public void photoUploadTest(){
+        // 1. https://the-internet.herokuapp.com/upload adresine gidelim
+        // 2. chooseFile butonuna basalim
+        // 3. Yuklemek istediginiz dosyayi secelim.
+        // 4. Upload butonuna basalim.
+        // 5. “File Uploaded!” textinin goruntulendigini test edelim.
+    }
+}
