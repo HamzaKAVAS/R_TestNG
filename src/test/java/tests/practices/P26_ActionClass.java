86510d4c5c4b4f5a71f3b6a81ea609b57e585f8c
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-26 12:21:22 +0300

    actionsClass

--- src/test/java/tests/practices/P26_ActionClass.java
+++ src/test/java/tests/practices/P26_ActionClass.java
@@ -1,6 +1,7 @@
 package tests.practices;
 
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.TOPracticePage;
 import utilities.ConfigReader;
@@ -27,9 +28,14 @@ public class P26_ActionClass {
         actions.dragAndDrop(toPracticePage.acceptableBoxElement,toPracticePage.dropHereBoxElement).perform();
         ReusableMethods.bekle(2);
 
-
         // 3- “Drop here” yazisi yerine “Dropped!” oldugunu test edin
+        String expectedText = "Dropped!";
+        String actualText = toPracticePage.dropHereBoxElement.getText();
+        Assert.assertEquals(actualText,expectedText);
+
         // 4- Sayfayi yenileyin
+        Driver.getDriver().navigate().refresh();
+
         // 5- “Not Acceptable” butonunu tutup “Drop Here” kutusunun ustune birakalim
         // 6- “Drop Here” yazisinin degismedigini test edin
         Driver.quitDriver();
