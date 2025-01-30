aa773a908710f9cc1e15fd06b0a740a6fb8a3ea6
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-30 16:03:37 +0300

    hamzaActions

--- src/test/java/tests/practices/P27_ActionsClass.java
+++ src/test/java/tests/practices/P27_ActionsClass.java
@@ -1,9 +1,11 @@
 package tests.practices;
 
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 import pages.DemoqaPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class P27_ActionsClass {
 
@@ -16,11 +18,15 @@ public class P27_ActionsClass {
         // 1- https://demoqa.com/droppable adresine gidelim
         Driver.getDriver().get(ConfigReader.getProperty("demoqaDroppableUrl"));
         DemoqaPage demoqaPage = new DemoqaPage();
+        Actions actions = new Actions(Driver.getDriver());
 
         // 2- “Drag me” butonunu tutup “Drop here” kutusunun ustune birakalim
-
+        actions.dragAndDrop(demoqaPage.dragMeBoxElement,demoqaPage.dropHereBoxElement).perform();
+        ReusableMethods.bekle(1);
 
         // 3- “Drop here” yazisi yerine “Dropped!” oldugunu test edin
+
+
         Driver.quitDriver();
     }
 }
