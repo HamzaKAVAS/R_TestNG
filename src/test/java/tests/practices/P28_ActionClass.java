540cb552e21e27cd84abff658684b1af66535a73
HamzaKAVAS <hmzkavas@gmail.com>
2025-01-31 20:46:17 +0300

    hamzaActions

---
+++ src/test/java/tests/practices/P28_ActionClass.java
@@ -0,0 +1,10 @@
+package tests.practices;
+
+public class P28_ActionClass {
+
+    // 1- https://www.testotomasyonu.com/ adresine gidin
+    // 2- “Kids Wear” menusunun acilmasi icin mouse’u bu menunun ustune getirin
+    // 3- “Boys” linkine basin
+    // 4- Acilan sayfadaki ilk urunu tiklayin
+    // 4- Acilan sayfada urun isminin “Boys Shirt White Color” oldugunu test edin
+}
