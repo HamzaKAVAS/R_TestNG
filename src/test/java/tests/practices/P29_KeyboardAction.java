2afab4a7448782a6684a721a115d8654356c0571
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-02 17:52:42 +0300

    KeyboardActions

--- src/test/java/tests/practices/P29_KeyboardAction.java
+++ src/test/java/tests/practices/P29_KeyboardAction.java
@@ -1,6 +1,8 @@
 package tests.practices;
 
 import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class P29_KeyboardAction {
 
@@ -11,7 +13,10 @@ public class P29_KeyboardAction {
     @Test
     public void keyboardActionsTest(){
         // 1- https://www.testotomasyonu.com sayfasina gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+
         // 2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin ve Enter’a basarak arama yaptirin
         // 3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
+        Driver.quitDriver();
     }
 }
