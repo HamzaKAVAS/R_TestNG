47faddf019101ca562dbe22a9c5b6ef787fdaaf0
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-02 18:06:14 +0300

    KeyboardActions

--- src/test/java/tests/practices/P29_KeyboardAction.java
+++ src/test/java/tests/practices/P29_KeyboardAction.java
@@ -1,8 +1,12 @@
 package tests.practices;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import pages.TestotomasyonuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class P29_KeyboardAction {
 
@@ -14,8 +18,18 @@ public class P29_KeyboardAction {
     public void keyboardActionsTest(){
         // 1- https://www.testotomasyonu.com sayfasina gidelim
         Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
+        TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
+        Actions actions = new Actions(Driver.getDriver());
 
         // 2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin ve Enter’a basarak arama yaptirin
+        actions.sendKeys(testotomasyonuPage.aramaKutusu)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("dell")
+                .keyDown(Keys.SPACE)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("c").perform();
+        ReusableMethods.bekle(1);
+
         // 3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
         Driver.quitDriver();
     }
