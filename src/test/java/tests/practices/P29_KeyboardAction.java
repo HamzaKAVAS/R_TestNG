633ea0d96109d04c02b4039c721d178875852a84
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-02 17:50:58 +0300

    KeyboardActions

--- src/test/java/tests/practices/P29_KeyboardAction.java
+++ src/test/java/tests/practices/P29_KeyboardAction.java
@@ -1,8 +1,17 @@
 package tests.practices;
 
+import org.testng.annotations.Test;
+
 public class P29_KeyboardAction {
 
     // 1- https://www.testotomasyonu.com sayfasina gidelim
     // 2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin ve Enter’a basarak arama yaptirin
     // 3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
+
+    @Test
+    public void keyboardActionsTest(){
+        // 1- https://www.testotomasyonu.com sayfasina gidelim
+        // 2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin ve Enter’a basarak arama yaptirin
+        // 3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
+    }
 }
