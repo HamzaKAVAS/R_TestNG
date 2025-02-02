d39a06e98565ce8dfd4474cf485454538d6b2e51
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-02 17:50:03 +0300

    KeyboardActions

---
+++ src/test/java/tests/practices/P29_KeyboardAction.java
@@ -0,0 +1,8 @@
+package tests.practices;
+
+public class P29_KeyboardAction {
+
+    // 1- https://www.testotomasyonu.com sayfasina gidelim
+    // 2- Arama kutusuna actions method’larini kullanarak “DELL Core I3” yazdirin ve Enter’a basarak arama yaptirin
+    // 3- Bulunan urun isminde “DELL Core I3” bulundugunu test edin
+}
