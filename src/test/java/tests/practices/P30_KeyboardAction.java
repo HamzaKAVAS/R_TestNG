5453bf5c9e75abeaeb7e50d4880328dadece6698
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-03 21:57:32 +0300

    KeyboardActions

---
+++ src/test/java/tests/practices/P30_KeyboardAction.java
@@ -0,0 +1,22 @@
+package tests.practices;
+
+import org.testng.annotations.Test;
+
+public class P30_KeyboardAction {
+
+    // 1- https://www.testotomasyonu.com adresine gidelim
+    // 2- Account linkine tiklayin
+    // 3- Sign Up linkine basalim
+    // 4- Ad, soyad, mail ve sifre kutularina deger yazalim ve Sign Up butonuna basalim
+    // 5- Kaydin olusturuldugunu test edin
+
+    @Test
+    public void testOtomasyonuRegisterTest(){
+        // 1- https://www.testotomasyonu.com adresine gidelim
+
+        // 2- Account linkine tiklayin
+        // 3- Sign Up linkine basalim
+        // 4- Ad, soyad, mail ve sifre kutularina deger yazalim ve Sign Up butonuna basalim
+        // 5- Kaydin olusturuldugunu test edin
+    }
+}
