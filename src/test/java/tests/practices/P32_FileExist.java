8452169741bd2d6c6d818355738801f81e9f6028
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-08 23:14:52 +0300

    fileExist

--- src/test/java/tests/practices/P32_FileExist.java
+++ src/test/java/tests/practices/P32_FileExist.java
@@ -1,10 +1,15 @@
 package tests.practices;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HerOkuPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class P32_FileExist {
 
@@ -13,16 +18,18 @@ public class P32_FileExist {
     // 3. Dosyanın başarıyla indirilip indirilmediğini test edelim
 
     @Test
-    public void fileExistTest(){
+    public void fileExistTest() {
         // 1. https://the-internet.herokuapp.com/download adresine gidelim.
         Driver.getDriver().get(ConfigReader.getProperty("herOkuDownload"));
         HerOkuPage herOkuPage = new HerOkuPage();
 
         // 2. logo.png dosyasını indirelim
         herOkuPage.picJpegElement.click();
-        ReusableMethods.bekle(2);
+        ReusableMethods.bekle(3);
 
         // 3. Dosyanın başarıyla indirilip indirilmediğini test edelim
+        String filePath = System.getProperty("user.home") + "\\Downloads\\pic.jpeg";
+        Assert.assertTrue(Files.exists(Paths.get(filePath)));
         Driver.quitDriver();
     }
 }
