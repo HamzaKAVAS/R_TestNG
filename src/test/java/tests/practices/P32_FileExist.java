fab783f7f5ab71a207f2850585b1452674e9f4ee
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-08 22:57:58 +0300

    fileExist

--- src/test/java/tests/practices/P32_FileExist.java
+++ src/test/java/tests/practices/P32_FileExist.java
@@ -2,4 +2,7 @@ package tests.practices;
 
 public class P32_FileExist {
 
+    // 1. https://the-internet.herokuapp.com/download adresine gidelim.
+    // 2. logo.png dosyasını indirelim
+    // 3. Dosyanın başarıyla indirilip indirilmediğini test edelim
 }
